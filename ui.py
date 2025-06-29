03a12a9c1c6848e979015bd50b1540f369179d8f
ethan0905 <ethansafar@gmail.com>
2025-06-29 13:10:51 +0200

    updated arrow color

--- ui.py
+++ ui.py
@@ -117,7 +117,7 @@ class ChatAgentWindow(NSWindow):
         send_btn.setTitle_("↑")
         send_btn.setBordered_(False)
         send_btn.setFont_(NSFont.boldSystemFontOfSize_(18))
-        send_btn.setContentTintColor_(NSColor.controlAccentColor())
+        send_btn.setContentTintColor_(NSColor.darkGrayColor())
         send_btn.setBackgroundColor_(NSColor.whiteColor())
         send_btn.setCornerRadius_(self.ARROW_SIZE / 2)
         vibrant.addSubview_(send_btn)
